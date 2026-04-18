d7cf4c4db3a6771e7f0587b81c39d5f41d5a6854
Mitsuru Utsugi <utsugi.mitsuru.5c@kyoto-u.ac.jp>
2026-04-18 22:44:27 +0900

    260418-iss003

--- example/Case1/run.sh
+++ example/Case1/run.sh
@@ -8,4 +8,4 @@ if [ ! -e "$root"/bin/l1tvinv ]; then
 fi
 
 rm -f log_case1
-"$root"/bin/l1tvinv -f input_mag.in -l -2. -g true.quide:1.e-4:1.:1. -c 1. -s settings.par -v 2>&1 | tee -a log_case1
+"$root"/bin/l1tvinv -f input_mag.in -l -2. -g true.guide:1.e-4:1.:1. -c 1. -s settings.par -v 2>&1 | tee -a log_case1
